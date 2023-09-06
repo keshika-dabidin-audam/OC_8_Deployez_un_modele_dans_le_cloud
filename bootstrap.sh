b3f202c68c94a3c3bc240b86ef050fc117dd05ee
keshika-dabidin-audam <keshika.dabidin@gmail.com>
2023-09-06 15:55:19 +0200

    first commit

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+sudo python3 -m pip install -U setuptools
+sudo python3 -m pip install -U pip
+sudo python3 -m pip install Pillow #==4.1.1 --no-cache-dir
+sudo python3 -m pip install keras #==2.13.1 --no-cache-dir
+# sudo python3 -m pip install tensorflow==1.13.1 --no-cache-dir
+sudo python3 -m pip install wrapt #==2.2.4 --no-cache-dir
+sudo python3 -m pip install pandas #==2.0.3 --no-cache-dir
+sudo python3 -m pip install findspark #==2.0.1 --no-cache-dir
+sudo python3 -m pip install pyarrow #==13.0.0 --no-cache-dir
+
